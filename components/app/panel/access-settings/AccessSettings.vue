70ae4bd339621bb68b3891318a8916f4f5611c1d
hahoseini <hosseinaliafg@gmail.com>
2022-11-14 01:44:35 +0330

    add machine info component to main components

--- components/app/panel/access-settings/AccessSettings.vue
+++ components/app/panel/access-settings/AccessSettings.vue
@@ -1,14 +1,69 @@
 <template>
-  <div class="rounded-lg border-2 border-primary">
+  <div class="rounded-lg border-2 border-primary regular">
     <div class="text-center bg-primary py-3">
-      <h1 class="text-white regular text-lg">تنظیمات دسترسی</h1>
+      <h1 class="text-white text-lg">تنظیمات دسترسی</h1>
     </div>
-    <div class="p-4"></div>
+    <div class="p-4">
+      <div class="p-3 rounded border border-blue-300 d-flex items-center">
+        <v-icon color="#60a5fa"> mdi-alert-circle-outline </v-icon>
+        <div class="mr-2 text-[#60a5fa] text-xl">
+          انتخاب یکی از گزینه های زیر اجباریست
+        </div>
+      </div>
+      <div class="custom-shadow p-4 rounded mt-10">
+        <h3 class="text-xl font-bold">فعال سازی رمز عبور</h3>
+        <div class="d-flex justify-between items-center">
+          <div>
+            با فعال کردن این قسمت یک رمز عبور تصادفی به ایمیل شما ارسال می شود.
+          </div>
+          <v-switch inset></v-switch>
+        </div>
+      </div>
+      <div class="custom-shadow p-4 rounded mt-10">
+        <h3 class="text-xl font-bold">افزودن کلید (SSH key)</h3>
+        <div class="d-flex justify-between items-center my-5">
+          <div>
+            برای دسترسی راحت تر به ماشین در این قسمت کلید خود را مشخص کنید.
+          </div>
+          <v-btn color="primary">افزودن کلید</v-btn>
+        </div>
+        <v-expansion-panels accordion>
+          <v-expansion-panel>
+            <v-expansion-panel-header
+              >مشاهده لیست کلید ها</v-expansion-panel-header
+            >
+            <v-expansion-panel-content>
+              <div v-for="i in 4" :key="i">
+                <v-radio-group v-model="radioGroup" hide-details>
+                  <div class="d-flex justify-between items-center">
+                    <v-radio :label="`Radio ${i}`" :value="i"></v-radio>
+                    <v-btn text color="red">
+                      <v-icon color="red">mdi-delete</v-icon>
+                    </v-btn>
+                  </div>
+                </v-radio-group>
+              </div>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      radioGroup: 1,
+    };
+  },
+  watch: {
+    radioGroup(val) {
+      console.log(val);
+    },
+  },
+};
 </script>
 
 <style>
---
+++ components/app/panel/machin-info/MachinInfo.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="rounded-lg border-2 border-primary regular">
+    <div class="text-center bg-primary py-3">
+      <h1 class="text-white text-lg">مشخصات ماشین</h1>
+    </div>
+    <div class="p-4">
+      <h3 class="text-xl font-bold">مرحله پایانی</h3>
+      <v-row class="mt-5">
+        <v-col cols="12" sm="4">
+          <h4 class="text-lg font-bold">تعداد ماشین ها</h4>
+          <div class="d-flex items-center mt-3">
+            <v-btn color="primary" height="60">
+              <span class="text-[24px]">+</span>
+            </v-btn>
+            <div
+              class="
+                d-flex
+                justify-center
+                items-center
+                border-2 border-primary
+                rounded
+                w-[110px]
+                h-[55px]
+                mx-2
+                select-none
+                text-[20px]
+              "
+            >
+              1
+            </div>
+            <v-btn color="primary">
+              <span class="text-[24px]">-</span>
+            </v-btn>
+          </div>
+        </v-col>
+        <v-col cols="12" sm="5">
+          <h4 class="text-lg font-bold">نام ماشین</h4>
+          <v-text-field outlined height="45" label="نام ماشین" class="mt-3" />
+        </v-col>
+        <v-col cols="12" sm="3">
+          <h4 class="text-lg font-bold">پروژه</h4>
+          <v-select
+            label="پروژه"
+            outlined
+            no-data-text="پروژه ای یافت نشد"
+            class="mt-3"
+          />
+        </v-col>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
