566c59f787e137aa057d2ba7ba846e04a8625e49
hahoseini <hosseinaliafg@gmail.com>
2022-11-29 09:55:14 +0330

    change and improve structure of components

---
+++ components/app/panel/projects/services/ProjectCloudServer.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="d-flex items-center justify-between my-[30px]">
+      <h2 class="text-[28px] font-bold">سرورهای ابری</h2>
+      <v-btn color="primary">سرور ابری جدید</v-btn>
+    </div>
+    <div
+      :class="[
+        loading || projectData.length === 0
+          ? 'd-flex justify-center items-center'
+          : null,
+        'min-h-[200px]',
+      ]"
+    >
+      <loading v-if="loading" />
+      <template v-if="!loading">
+        <cloud-servers v-for="(i, index) in projectData" :key="index" />
+        <div v-if="projectData.length === 0">
+          <h1 class="regular text-xl">اطلاعاتی یافت نشد!</h1>
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+import CloudServers from "../../cloud-servers/CloudServers.vue";
+export default {
+  components: { CloudServers },
+  data() {
+    return {
+      projectData: ["ali", "ali"],
+      loading: true,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 4000);
+  },
+};
+</script>
+
+<style>
+</style>
--- components/app/panel/volumes/HardDisks.vue
+++ components/app/panel/volumes/HardDisks.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <div class="d-flex items-center justify-between my-[30px]">
-      <h2 class="text-[28px] font-bold">هارد دیسک ها</h2>
-      <v-btn color="primary">هارد دیسک جدید</v-btn>
-    </div>
-  </div>
+  <div></div>
 </template>
 
 <script>
