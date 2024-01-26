4d56476204027a434557c48c39bcf38dfc04de9e
Alireza Alami <AlirezaAlami2949@gmail.com>
2024-01-26 19:50:33 +0330

    add user profile update

--- pages/panel/profile/index.vue
+++ pages/panel/profile/index.vue
@@ -11,7 +11,7 @@
           lazy-validation
           ref="form"
           v-model="validForm"
-          @submit.prevent=""
+          @submit.prevent="updateProfile"
         >
           <v-row class="mt-5">
             <v-col cols="12" md="6" class="py-0">
@@ -19,74 +19,161 @@
                 outlined
                 label="نام"
                 placeholder="نام خود را وارد کنید"
+                v-model="user.first_name"
               />
             </v-col>
-            <v-col cols="12" md="6" class="py-0"
-              ><v-text-field
+            <v-col cols="12" md="6" class="py-0">
+              <v-text-field
                 outlined
                 label="نام خانوادگی"
                 placeholder="نام خانوادگی خود را وارد کنید"
-            /></v-col>
-            <v-col cols="12" md="6" class="py-0"
-              ><v-text-field
+                v-model="user.last_name"
+              />
+            </v-col>
+            <v-col cols="12" md="6" class="py-0">
+              <v-text-field
                 outlined
                 label="ایمیل"
                 placeholder="ایمیل خود را وارد کنید"
-            /></v-col>
-            <v-col cols="12" md="6" class="py-0"
-              ><v-text-field
+                v-model="user.email"
+                readonly
+              />
+            </v-col>
+            <v-col cols="12" md="6" class="py-0">
+              <v-text-field
                 outlined
                 label="شماره ملی"
                 placeholder="شماره ملی خود را وارد کنید"
-            /></v-col>
-            <v-col cols="12" md="6" class="py-0"
-              ><v-text-field
+                v-model="user.national_id"
+                :rules="[rules.required, rules.nationalId]"
+              />
+            </v-col>
+            <v-col cols="12" md="6" class="py-0">
+              <v-text-field
                 outlined
                 label="شماره همراه"
                 placeholder="شماره همراه خود را وارد کنید"
-            /></v-col>
+                v-model="user.phone_number"
+                :rules="[rules.phoneNumber]"
+              />
+            </v-col>
           </v-row>
           <div class="d-flex flex-row-reverse">
-            <v-btn type="submit" color="primary" height="55"
-              >ذخیره اطلاعات</v-btn
-            >
+            <v-btn type="submit" color="primary" height="55">
+              ذخیره اطلاعات
+            </v-btn>
           </div>
         </v-form>
       </div>
       <v-divider></v-divider>
       <div class="py-8">
-        <h1 class="regular text-lg">تایید شماره موبایل</h1>
-        <div class="d-flex justify-between items-start mt-4">
-          <div class="d-flex items-center">
-            <v-icon color="black">mdi-square-rounded</v-icon>
-            <span class="mr-2 regular"
-              >در این قسمت شما میتوانید شماره موبایلی که با آن حساب خود را ایجاد
-              کرده اید تغییر دهید.</span
-            >
-          </div>
-          <v-btn height="55" color="primary">تغییر شماره موبایل</v-btn>
-        </div>
-      </div>
-      <v-divider></v-divider>
-      <div class="py-8">
         <h1 class="regular text-lg">تغییر رمز عبور</h1>
-        <div class="d-flex justify-between items-start mt-4">
-          <div class="d-flex items-center">
-            <v-icon color="black">mdi-square-rounded</v-icon>
-            <span class="mr-2 regular"
-              >در این قسمت شما میتوانید رمز عبور حساب خود را تغییر دهید.</span
-            >
+        <v-form
+          lazy-validation
+          ref="form"
+          v-model="validForm"
+          @submit.prevent="changePassword"
+        >
+          <v-row class="mt-5">
+            <v-col cols="12" md="6" class="py-0">
+              <v-text-field
+                outlined
+                label="رمز عبور جدید"
+                placeholder="رمز عبور جدید را وارد کنید"
+                v-model="password.new_password"
+                type="password"
+              />
+            </v-col>
+          </v-row>
+          <div class="d-flex flex-row-reverse">
+            <v-btn type="submit" color="primary" height="55">
+              تغییر رمز عبور
+            </v-btn>
           </div>
-          <v-btn height="55" color="primary">تغییر رمز عبور</v-btn>
-        </div>
+        </v-form>
       </div>
     </div>
+
+    <snack-bar />
   </v-container>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   layout: "panel",
+  data() {
+    return {
+      validForm: false,
+      rules: {
+        required: (value) => !!value || "این فیلد اجباری است.",
+        nationalId: (value) =>
+          /^(?:\d{10}|\d{12})$/.test(value) || "شماره ملی معتبر نیست.",
+        phoneNumber: (value) =>
+          value === '' || value === undefined || /^(09\d{9})$/.test(value) || 'شماره همراه معتبر نیست.',
+      },
+
+      user: {
+        id: null,
+        first_name: "",
+        last_name: "",
+        email: "",
+        national_id: "",
+        phone_number: "",
+      },
+      password: {
+        new_password: "",
+      },
+    };
+  },
+  mounted() {
+    this.fetchUserProfile();
+  },
+  methods: {
+    async fetchUserProfile() {
+      try {
+        const response = await this.$get("users/profile");
+        this.user = response.data;
+      } catch (error) {
+        console.error("Error fetching user profile:", error);
+      }
+    },
+    async updateProfile() {
+      try {
+        const response = await this.$put("users/profile/", this.user);
+        console.log("Profile updated successfully:", response.data);
+        this.$store.dispatch("SnackBar/show", {
+          text: "اطلاعات با موفقیت به‌روز شد.",
+          type: "success",
+        });
+      } catch (error) {
+        console.error("Error updating user profile:", error);
+        this.$store.dispatch("SnackBar/show", {
+          text: "مشکلی در به‌روزرسانی اطلاعات به وجود آمد.",
+          type: "error",
+        });
+      }
+    },
+    async changePassword() {
+      try {
+        const response = await this.$put(
+          "users/change-password/",
+          this.password
+        );
+        this.$store.dispatch("SnackBar/show", {
+          text: "اطلاعات با موفقیت به‌روز شد.",
+          type: "success",
+        });
+      } catch (error) {
+        console.error("Error changing password:", error);
+        this.$store.dispatch("SnackBar/show", {
+          text: "مشکلی در تغییر رمز عبور به وجود آمد.",
+          type: "error",
+        });
+      }
+    },
+  },
 };
 </script>
 
